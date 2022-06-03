8bbe9e979263cb89b29933cef5bb03d619e6860d
Adrian <64158136+dricastcode@users.noreply.github.com>
2022-06-03 00:04:24 -0700

    Updated babel, added app js

---
+++ src/app.js
@@ -0,0 +1,2 @@
+//Testing commit to GitHub
+alert("Welcome! This page is a capstone project through Udacity's Front End Developer course. Enter your destination to see projected weather and a view of your city")
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './styles/main.scss'
 import chicagoView from './img/chicagoview.jpeg'
+import './app'
 
 const chicago = document.getElementById('chicagoView')
 chicago.src = chicagoView
